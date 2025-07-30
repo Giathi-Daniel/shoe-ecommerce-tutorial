8988bffc33e1c5ecd5b3e8c61bdaba5da328c856
Daniel Giathi <giathidaniel252@gmail.com>
2025-07-30 23:30:18 +0300

    Add default pages and implement Zenquotes for a random quote in our NotFound page

---
+++ shoe-ecommerce-frontend/src/routes.jsx
@@ -0,0 +1,19 @@
+import Home from './pages/Home';
+import NotFound from './pages/NotFound'
+// import ProductDetails from './pages/ProductDetails';
+// import Cart from './pages/Cart';
+// import Checkout from './pages/Checkout';
+// import Login from './pages/Login';
+// import Signup from './pages/Signup';
+
+const routes = [
+  { path: '/', element: <Home /> },
+  { path: '*', element: <NotFound /> }
+//   { path: '/product/:id', element: <ProductDetails /> },
+//   { path: '/cart', element: <Cart /> },
+//   { path: '/checkout', element: <Checkout /> },
+//   { path: '/login', element: <Login /> },
+//   { path: '/signup', element: <Signup /> },
+];
+
+export default routes;
